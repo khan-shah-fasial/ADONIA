32ef383f0c26e34c4fc644e9c8bde85eb7ccaa42
Faisal <khanfasial.makent@gmail.com>
2024-05-23 15:37:15 +0530

    update faisal code in bussiness

--- app/Http/Controllers/backend/AboutController.php
+++ app/Http/Controllers/backend/AboutController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class AboutController extends Controller
 {
@@ -14,27 +15,99 @@ class AboutController extends Controller
         return view('backend.pages.about.index');
     }
 
-    public function home_banner(){
+    public function about_banner(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'Banner' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'notification' => $validator->errors()->all()
+            ], 200);
+        }
+        
+        $file = $request->file('Banner');
+        $bannerPath = $file->store('assets/banner/', 'public');
+
+        $result = DB::table('pages')->where('page_name', $request->page)->update([
+            'banners' => $bannerPath,
+        ]);
+
+        if($result){
+            $response = [
+                'status' => true,
+                'notification' => 'About Banners Save successfully!',
+            ];
+        } else {
+            $response = [
+                'status' => false,
+                'notification' => 'Somthing Went Wrong!',
+            ];
+        }
+
+        return response()->json($response);
 
     }
 
-    public function home_intro(){
+    public function about_intro(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'intro' => 'required',
+            'partner' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'notification' => $validator->errors()->all()
+            ], 200);
+        }   
+
+        $intro_data = $request->intro;
+        $partner_data = $request->partner;
+
+        $intro = [
+            'intro' => $intro_data,
+            'partner' => $partner_data
+        ];
+
+        $result = DB::table('pages')->where('page_name', $request->page)->update([
+            'introduction' => json_encode($intro),
+        ]);
+
+        if($result){
+            $response = [
+                'status' => true,
+                'notification' => 'About Intro Save successfully!',
+            ];
+        } else {
+            $response = [
+                'status' => false,
+                'notification' => 'Somthing Went Wrong!',
+            ];
+        }
+
+        return response()->json($response);
         
     }
 
-    public function home_marque(){
+    public function about_steps(Request $request){
+
+
         
     }
 
-    public function home_business(){
+    public function about_business(Request $request){
 
     }
 
-    public function home_counter(){
+    public function about_counter(Request $request){
 
     }
 
-    public function home_project(){
+    public function about_project(Request $request){
 
     }
 
